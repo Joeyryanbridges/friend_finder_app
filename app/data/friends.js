7f274a6c5d20f97d6e4985309c914b0e97f94914
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-04-23 22:46:00 -0700

    updated friends.js, working on routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,163 +1,163 @@
 var friendsArr = [
 
     {   //1
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "James Jenkins",
+        "photo": "http://www.joblo.com/newsimages1/JamesMarsdenHeadshot.jpg",
         "scores": [
-            5,
+            3,
             1,
             4,
             4,
-            5,
-            1,
-            2,
-            5,
             4,
+            1,
+            3,
+            5,
+            5,
             1
         ]
     },
     {   //2
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Roger Rapers",
+        "photo": "https://i.pinimg.com/originals/36/67/33/366733ea4e6dd87f4f2e348b1b30d733.jpg",
         "scores": [
             5,
             1,
             4,
-            4,
+            3,
             5,
             1,
+            3,
             2,
-            5,
             4,
-            1
+            5
         ]
     },
     {   //3
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Henry Buttkissenger",
+        "photo": "https://pbs.twimg.com/media/Cs6zPVUXYAA-u_g.jpg",
         "scores": [
-            5,
+            2,
             1,
             4,
             4,
             5,
-            1,
+            4,
             2,
             5,
-            4,
+            5,
             1
         ]
     },
     {   //4
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Sam Littlethingy",
+        "photo": "https://pbs.twimg.com/media/Cs6zPVUXYAA-u_g.jpg",
         "scores": [
-            5,
+            3,
             1,
             4,
-            4,
+            3,
             5,
             1,
-            2,
+            3,
             5,
-            4,
+            3,
             1
         ]
     },
     {   //5
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Partsy Lameass",
+        "photo": "http://cdn.smosh.com/sites/default/files/ftpuploads/bloguploads/0413/bowl-part-stache.jpg",
         "scores": [
-            5,
+            2,
             1,
             4,
             4,
-            5,
+            4,
             1,
             2,
             5,
-            4,
+            5,
             1
         ]
     },
     {   //6
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Rachel Dimebags",
+        "photo": "https://kyec8tsl37-flywheel.netdna-ssl.com/wp-content/uploads/2016/01/1980s-3.jpg",
         "scores": [
-            5,
+            3,
             1,
             4,
             4,
-            5,
+            2,
             1,
             2,
-            5,
             4,
+            5,
             1
         ]
     },
     {   //7
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Cynthia Spikez",
+        "photo": "https://i.pinimg.com/originals/ad/c3/eb/adc3ebe65ae64416bc440cc6e7812f46.jpg",
         "scores": [
             5,
-            1,
+            3,
             4,
             4,
             5,
             1,
             2,
             5,
-            4,
+            2,
             1
         ]
     },
     {   //8
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Mariah SnaggleTooth",
+        "photo": "https://gfx-bloggar.aftonbladet-cdn.se/wp-content/blogs.dir/428/files/2017/03/Skarmavbild-2017-03-29-kl.-07.58.51-1024x999.png",
         "scores": [
-            5,
-            1,
             4,
-            4,
-            5,
-            1,
             2,
+            3,
+            4,
+            5,
+            1,
+            3,
             5,
             4,
-            1
+            2
         ]
     },
     {   //9
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Geez Lousie",
+        "photo": "https://i.pinimg.com/originals/7b/f8/f3/7bf8f3b6e3ee36e6105f797deea767df.jpg",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
+            3,
+            3,
+            2,
             2,
             5,
-            4,
+            5,
+            2,
+            3,
+            3,
             1
         ]
     },
     {   //10
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Tina Scream-a",
+        "photo": "https://1.bp.blogspot.com/-yBfM17mAJLc/WL7bTKT073I/AAAAAAACmI0/l8Pg06YZ9Ts5J5JquBwcvsjf7-jguFFhgCLcB/s1600/bad-glamour-shots-1.jpg",
         "scores": [
             5,
-            1,
-            4,
+            2,
+            5,
             4,
             5,
             1,
             2,
-            5,
-            4,
-            1
+            2,
+            3,
+            4
         ]
     }
 ]
